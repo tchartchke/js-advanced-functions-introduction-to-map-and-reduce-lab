163ad7e9db21613995ae2ddb85331c06100b842a
tchartchke <chartai.kl@gmail.com>
2021-04-19 14:26:59 -0500

    commit tree 225305ba49a394d6829207fc6383b27a6347b065

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function mapToNegativize(sourceArray){
+  return sourceArray.map(x => x* -1 );
+}
+function mapToNoChange(sourceArray){
+  return sourceArray.map(x => x );
+}
+function mapToDouble(sourceArray){
+  return sourceArray.map( x=> x*2 );
+}
+function mapToSquare(sourceArray){
+  return sourceArray.map( x=> x**2 );
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+  return sourceArray.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+  }, startingPoint)
+}
+function reduceToAllTrue(sourceArray){
+  return sourceArray.reduce((accumulator, currentValue) => {
+    return !!(accumulator && currentValue)
+  }, true)
+}
+function reduceToAnyTrue(sourceArray){
+  return sourceArray.reduce((accumulator, currentValue) => {
+    return !!(accumulator || currentValue)
+  }, false)
+}
